59806a3ffeac259b5bf5799f11e71e39cf7fe254
ravenpuk <vare_16@hotmail.com>
2023-08-03 07:40:29 +0200

    added tictactoe in menu

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import os
 
 print("Hello world")
 
+def tictactoe():
+	pass
+
 def menu():
+
+	a = True
 
 	os.system('clear')
 	print("\n Please make your selection :")
 	print("\n ")
 	print("\n 0 : Continue")
-	print("\n 1 : Exit")
+	print("\n 1 : Tic-Tac-Toe")
+	print("\n 2 : Exit")
 
 	a = input("make your selection")
-	if a == "0":
-		b=True
-	elif a =="1":
+	
+	if a =="2":
 		b=False
 	else:
-		b=True
+		if b == 1:
+			tictactoe()
 
 	return b
 
